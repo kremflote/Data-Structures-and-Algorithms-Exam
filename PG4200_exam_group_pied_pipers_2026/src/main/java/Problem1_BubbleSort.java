fa68c048cf3195dac921ef268289832ed0089428
kremfote <flyingnorwegians@gmail.com>
2026-03-24 16:11:44 +0100

    deoptimized bubblesort

--- PG4200_exam_group_pied_pipers_2026/src/main/java/Problem1_BubbleSort.java
+++ PG4200_exam_group_pied_pipers_2026/src/main/java/Problem1_BubbleSort.java
@@ -8,7 +8,7 @@ public class Problem1_BubbleSort {
     public static void bubbleSortNonOptimised(ArrayList<Wine> list) {
         int n = list.size();
         for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
+            for (int j = 0; j < n - 1; j++) {
                 if (list.get(j).alcohol() > list.get(j + 1).alcohol()) {
                     Wine temp = list.get(j);
                     list.set(j, list.get(j + 1));
@@ -18,7 +18,7 @@ public class Problem1_BubbleSort {
         }
     }
 
-    // Optimised: exits early if no swaps happen in a pass
+    // Optimised: exits early if no swaps happen in a pass, and reduces range for each pass
     public static void bubbleSortOptimised(ArrayList<Wine> list) {
         int n = list.size();
         for (int i = 0; i < n - 1; i++) {
