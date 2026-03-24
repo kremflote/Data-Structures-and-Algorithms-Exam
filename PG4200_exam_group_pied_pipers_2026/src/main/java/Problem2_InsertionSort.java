4430489d6df4beabffbd7e208b463456bc5e86cc
mariusaellingsen <marius.a.ellingsen@gmail.com>
2026-03-24 19:39:59 +0100

    fixed void stuff

--- PG4200_exam_group_pied_pipers_2026/src/main/java/Problem2_InsertionSort.java
+++ PG4200_exam_group_pied_pipers_2026/src/main/java/Problem2_InsertionSort.java
@@ -3,19 +3,16 @@ package main.java;
 import java.util.ArrayList;
 
 public class Problem2_InsertionSort {
-    public static int insertionSort(ArrayList<Wine> list) {
-        int comparisons = 0;
+    public static void insertionSort(ArrayList<Wine> list) {
         for (int i = 1; i < list.size(); i++) {
             Wine key = list.get(i);
             double keyAlcohol = key.alcohol();
             int j = i - 1;
             while (j >= 0 && list.get(j).alcohol() > keyAlcohol) {
-                comparisons++;
                 list.set(j + 1, list.get(j));
                 j--;
             }
             list.set(j + 1, key);
         }
-        return comparisons;
     }
 }
