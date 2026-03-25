612e04eb59380e48f2f768c32473799e3b62bbd1
mariusaellingsen <marius.a.ellingsen@gmail.com>
2026-03-25 14:47:39 +0100

    removed count functionality from mergesort

--- PG4200_exam_group_pied_pipers_2026/src/main/java/Problem3_MergeSort.java
+++ PG4200_exam_group_pied_pipers_2026/src/main/java/Problem3_MergeSort.java
@@ -4,33 +4,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Problem3_MergeSort {
+
+    // Wrapper method so our benchmark tool can call the recursive method
     public static void mergeSort(ArrayList<Wine> list) {
         mergeSort(list, 0, list.size() - 1);
     }
-    public static int mergeSort(ArrayList<Wine> list, int left, int right) {
-        if (left >= right) return 0; // If there is only 1 element, already sorted. Return.
-        int mid = left + (right - left) / 2; // Find the middle
-        int count = mergeSort(list, left, mid) // Sort the left half
-                + mergeSort(list, mid + 1, right); // sort the right half
-        count += merge(list, left, mid, right); // Merge them together
-        return count;
+    private static void mergeSort(ArrayList<Wine> list, int left, int right) {
+        if (left >= right) return;
+
+        int mid = left + (right - left) / 2;
+
+        mergeSort(list, left, mid);
+        mergeSort(list, mid + 1, right);
+
+        merge(list, left, mid, right);
     }
 
-    private static int merge(ArrayList<Wine> list, int left, int mid, int right) {
+    private static void merge(ArrayList<Wine> list, int left, int mid, int right) {
         List<Wine> temp = new ArrayList<>();
-        int i = left, j = mid + 1, count = 0;
+
+        int i = left, j = mid + 1;
+
         while (i <= mid && j <= right) {
             if (list.get(i).alcohol() <= list.get(j).alcohol()) {
-                temp.add(list.get(i++)); // Take the smallest value from left half
+                temp.add(list.get(i++));
             } else {
-                temp.add(list.get(j++)); // Take the smallest value from the right half
-                count++;
+                temp.add(list.get(j++));
             }
         }
-        while (i <= mid) temp.add(list.get(i++)); // Checks for leftover elements in left half
-        while (j <= right) temp.add(list.get(j++)); // Checks for leftover elements in right half
-        for (int k = 0; k < temp.size(); k++)
+
+        while (i <= mid) temp.add(list.get(i++));
+        while (j <= right) temp.add(list.get(j++));
+
+        for (int k = 0; k < temp.size(); k++) {
             list.set(left + k, temp.get(k));
-        return count;
+        }
     }
 }
