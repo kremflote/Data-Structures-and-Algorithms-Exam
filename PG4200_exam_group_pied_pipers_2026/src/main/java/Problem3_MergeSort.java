b86abb9afc1ba429c00c5d362b9ac0645a80cd9c
mariusaellingsen <marius.a.ellingsen@gmail.com>
2026-03-25 14:56:59 +0100

    polished comments in MergeSort

--- PG4200_exam_group_pied_pipers_2026/src/main/java/Problem3_MergeSort.java
+++ PG4200_exam_group_pied_pipers_2026/src/main/java/Problem3_MergeSort.java
@@ -9,17 +9,22 @@ public class Problem3_MergeSort {
     public static void mergeSort(ArrayList<Wine> list) {
         mergeSort(list, 0, list.size() - 1);
     }
+
+    // Merge Sort Implementation. Time complexity: O(n log n) in all cases.
+    // Splits the list in half until each sublist contains one element
+    // Then it merges the sublists back together in sorted order
     private static void mergeSort(ArrayList<Wine> list, int left, int right) {
-        if (left >= right) return;
+        if (left >= right) return;              // If there is only 1 element, already sorted. Return.
 
-        int mid = left + (right - left) / 2;
+        int mid = left + (right - left) / 2;    // Find the middle
 
-        mergeSort(list, left, mid);
-        mergeSort(list, mid + 1, right);
+        mergeSort(list, left, mid);             // Sort left half
+        mergeSort(list, mid + 1, right);    // sort right half
 
-        merge(list, left, mid, right);
+        merge(list, left, mid, right);          // Merge sorted halves
     }
 
+    // Merges two already sorted halves into a single sorted section
     private static void merge(ArrayList<Wine> list, int left, int mid, int right) {
         List<Wine> temp = new ArrayList<>();
 
@@ -27,14 +32,13 @@ public class Problem3_MergeSort {
 
         while (i <= mid && j <= right) {
             if (list.get(i).alcohol() <= list.get(j).alcohol()) {
-                temp.add(list.get(i++));
+                temp.add(list.get(i++));        // Take the smallest value from left half
             } else {
-                temp.add(list.get(j++));
+                temp.add(list.get(j++));        // Take the smallest value from the right half
             }
         }
-
-        while (i <= mid) temp.add(list.get(i++));
-        while (j <= right) temp.add(list.get(j++));
+        while (i <= mid) temp.add(list.get(i++));   // Checks for leftover elements in left half
+        while (j <= right) temp.add(list.get(j++)); // Checks for leftover elements in right half
 
         for (int k = 0; k < temp.size(); k++) {
             list.set(left + k, temp.get(k));
