b3e7330909e963c85032f98fdb98f8861efaeec4
mariusaellingsen <marius.a.ellingsen@gmail.com>
2026-03-25 08:19:36 +0100

    added comments to insertion sort method

--- PG4200_exam_group_pied_pipers_2026/src/main/java/Problem2_InsertionSort.java
+++ PG4200_exam_group_pied_pipers_2026/src/main/java/Problem2_InsertionSort.java
@@ -3,16 +3,25 @@ package main.java;
 import java.util.ArrayList;
 
 public class Problem2_InsertionSort {
+
     public static void insertionSort(ArrayList<Wine> list) {
+
+        // Start on the second index as the first is already sorted
         for (int i = 1; i < list.size(); i++) {
-            Wine key = list.get(i);
-            double keyAlcohol = key.alcohol();
+
+            // Store a copy of the current wine
+            Wine current_wine = list.get(i);
+            double keyAlcohol = current_wine.alcohol();
+
+            // Move all elements with higher alcohol content than the current wine to the right
             int j = i - 1;
             while (j >= 0 && list.get(j).alcohol() > keyAlcohol) {
                 list.set(j + 1, list.get(j));
                 j--;
             }
-            list.set(j + 1, key);
+
+            // set the current wine-element in the correct place in the sorted list
+            list.set(j + 1, current_wine);
         }
     }
 }
