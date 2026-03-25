c38e68701024b326ac3b38e26fe162f8a943292e
mariusaellingsen <marius.a.ellingsen@gmail.com>
2026-03-25 11:20:31 +0100

    added comments

--- PG4200_exam_group_pied_pipers_2026/src/main/java/Problem1_BubbleSort.java
+++ PG4200_exam_group_pied_pipers_2026/src/main/java/Problem1_BubbleSort.java
@@ -6,7 +6,12 @@ public class Problem1_BubbleSort {
 
     public static void bubbleSortNonOptimised(ArrayList<Wine> list) {
         int n = list.size();
+
+        // Grabs an element in the list
         for (int i = 0; i < n - 1; i++) {
+
+            // Compares that element to the adjacent element, swap places if it's bigger than the adjacent one
+            // Repeat for-loop until it reaches end of list, even though no swaps occured
             for (int j = 0; j < n - 1; j++) {
                 if (list.get(j).alcohol() > list.get(j + 1).alcohol()) {
                     Wine temp = list.get(j);
@@ -20,16 +25,26 @@ public class Problem1_BubbleSort {
     // Optimised: exits early if no swaps happen in the outer pass, and reduces range for each inner pass
     public static void bubbleSortOptimised(ArrayList<Wine> list) {
         int n = list.size();
+
+        // Grabs an element in the list
         for (int i = 0; i < n - 1; i++) {
+
+            // Flag to track if the current element swapped places or not
             boolean swapped = false;
+
+            // Compares that element to the adjacent element, swap places if it's bigger than the adjacent one
             for (int j = 0; j < n - i - 1; j++) {
                 if (list.get(j).alcohol() > list.get(j + 1).alcohol()) {
                     Wine temp = list.get(j);
                     list.set(j, list.get(j + 1));
                     list.set(j + 1, temp);
+
+                    // Tell algorithm that the element was swapped, make the loop run again
                     swapped = true;
                 }
             }
+            // Abort if no swaps occurred, check next element in outer loop
+            // If list is ordered, the inner loop will never run
             if (!swapped) break;
         }
     }
